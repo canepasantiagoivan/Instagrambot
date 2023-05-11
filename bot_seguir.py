ea8f411009b1d118a9ccb1ac3ccbd1a898bb6fcf
Santiago Canepa <125301263+canepasantiagoivan@users.noreply.github.com>
2023-05-11 07:59:55 -0300

    Update bot_seguir.py

--- bot_seguir.py
+++ bot_seguir.py
@@ -16,13 +16,12 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 
-# Define custom headers
+# Define custom headers para evitar ser identificado por IG
 headers = {
     "User-Agent": "Mozilla/4.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0",
     "Accept-Language": "en-US,en;q=0.9",
 }
 
-# Add headers to Chrome options
 chrome_options = Options()
 for header, value in headers.items():
     chrome_options.add_argument(f"--header-{header}={value}")
@@ -31,7 +30,7 @@ for header, value in headers.items():
 driver = webdriver.Chrome(executable_path="path/to/chromedriver", options=chrome_options)
 
  
-# Define the proxy server 
+# proxy server 
 PROXY = "IpOfTheProxy:PORT" 
  
 # Set ChromeOptions() 
@@ -40,19 +39,13 @@ chrome_options = webdriver.ChromeOptions()
 # Add the proxy as argument 
 chrome_options.add_argument("--proxy-server=%s" % PROXY) 
 
-# Your code to interact with the page goes here
-
-# Don't forget to close the driver
-
-
-# Set up a WebDriver
 driver_path = 'path/to/chromedriver.exe'  # Change this to the path of your WebDriver
 driver = webdriver.Chrome(executable_path=driver_path)
 
 # Log into Instagram
 
-username = 'xxx1'  # Change this to your Instagram username
-password = 'xxx1'  # Change this to your Instagram password
+username = 'introducir usuario'  # Change this to your Instagram username
+password = 'introducir pass'  # Change this to your Instagram password
 driver.get('https://www.instagram.com/accounts/login/')
 time.sleep(4)
 username_field = driver.find_element('name', 'username')
@@ -64,23 +57,27 @@ login_button = driver.find_element('xpath', "//button[@type='submit']")
 login_button.click()
 time.sleep(random.randint(5, 7))
 
-#username='so.manenti'
-username = 'axelalmaraz77'
-# Navigate to your profile
-profile_url =f'https://www.instagram.com/{username}/'
-driver.get(profile_url)
-time.sleep(random.randint(6, 8))
-driver.find_element(By.XPATH, '//a[contains(@href, "followers")]').click()
 
-time.sleep(random.randint(4, 6))
-followers_list = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]')
+username = 'introducir usuario a copiar'
+
+time.sleep(random.randint(6, 8))
+
+driver.get("https://www.instagram.com/" + username + "/following/")
+time.sleep(5)
+
+
+time.sleep(5)
+following_list = driver.find_element('xpath', '/html/body/div[2]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]')
 #followers_list = driver.find_element('xpath', '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]')
 scroll_height = 0
 
 
 
 
-'''while True:
+
+'''
+
+while True:
     
     buttons = driver.find_elements('xpath',"//div[contains(@class, 'x9f619') and contains(@class, 'xjbqb8w') and contains(@class, 'x78zum5') and contains(@class, 'x168nmei') and contains(@class, 'x13lgxp2') and contains(@class, 'x5pf9jr') and contains(@class, 'xo71vjh') and contains(@class, 'x16n37ib') and contains(@class, 'x1uhb9sk') and contains(@class, 'x1plvlek') and contains(@class, 'xryxfnj') and contains(@class, 'xs83m0k') and contains(@class, 'x1c4vz4f') and contains(@class, 'x1q0g3np') and contains(@class, 'xqjyukv') and contains(@class, 'x1qjc9v5') and contains(@class, 'x1oa3qoh') and contains(@class, 'x1nhvcw1')]//button//div[contains(text(), 'Seguir')]")
 
@@ -99,11 +96,12 @@ scroll_height = 0
 
 
 
-# Cargar el modelo desde el archivo
+# Cargar el modelo desde el archivo, esto es solo si se piensa en seguir personas de un genero determinado de lo contrario es necesario comentar las secciones. 
+# Asi como esta, solo seguiria nombre de usuarios que  que prediga como femeninos
+
 with open('./modelo.pkl', 'rb') as f:
     modelo = pickle.load(f)
 
-# Hacer inferencias con el modelo
 def predict_gender(name):
     features = feature_extractor(name.lower())
     X = vectorizer.transform([features])
@@ -111,6 +109,8 @@ def predict_gender(name):
     return "hombre" if prediction[0] == 1 else "mujer"
 # nueva_prediccion = predict_gender('sofia')
 
+
+# A continuacion el codigo creara un CSV con cada usuario al cual se siga o se le envie solicitud, el mismo codigo leera posteriormete el csv antes de seguir a cada usuario, con el objetivo de no repetir solicitudes
 def guardar_usuario_csv(usuario):
     fecha_actual = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     with open('usuarios.csv', mode='a', newline='', encoding='utf-8') as file:
@@ -133,17 +133,61 @@ def usuario_ya_guardado(usuario):
                 return True
     return False    
 
+contador = 0
 contador_total = 0
 contador_tanda = 0
 scrolls = 0
+from name_predictor.separetor import possible_name
 
 
-while True:
+while contador_total<600 :
+
+    casillas_grandes = driver.find_elements(By.XPATH,"//div[contains(@class, 'x9f619') and contains(@class, 'xjbqb8w') and contains(@class, 'x78zum5') and contains(@class, 'x168nmei') and contains(@class, 'x13lgxp2') and contains(@class, 'x5pf9jr') and contains(@class, 'xo71vjh') and contains(@class, 'x1pi30zi') and contains(@class, 'x1swvt13') and contains(@class, 'xwib8y2') and contains(@class, 'x1y1aw1k') and contains(@class, 'x1uhb9sk') and contains(@class, 'x1plvlek') and contains(@class, 'xryxfnj') and contains(@class, 'x1c4vz4f') and contains(@class, 'x2lah0s') and contains(@class, 'xdt5ytf') and contains(@class, 'xqjyukv') and contains(@class, 'x1qjc9v5') and contains(@class, 'x1oa3qoh') and contains(@class, 'x1nhvcw1')]")
+
+
+    for idx, casilla in enumerate(casillas_grandes):
+        contador += 1 
+        
+        if contador > 10*(contador_tanda+1):
+            contador_tanda += 1
+            contador = 0
+            break
+            
+
+        usuario = casilla.text.split('\n')[0]
+        nombre_usuario = casilla.text.split('\n')[1]
+        print(nombre_usuario.encode("cp1252", errors="replace").decode("cp1252"))
+
+# Ejecutamos la prediccion antes de seguir, nuevamente COMENTAR ESTA LINEA SI NO SE QUIERE SEGMENTAR POR GENERO.
+        if predict_gender(possible_name(nombre_usuario)) == "mujer":
+            # print(possibl_name(nombre_usuario), "es mujer")
+            try:
+                
+                #boton_siguiendo = casilla.find_element(By.XPATH, ".//div[contains(@class, 'x9f619') and contains(@class, 'x1q0g3np') and contains (@class, 'xqjyukv') and contains (@class, 'x1nhvcw1') and contains (@class, 'x1oa3qoh')]//button//div[contains(text(), 'Seguir')]")
+                boton_siguiendo = casilla.find_elements(By.XPATH, ".//button[contains(@class, '_acan') and contains(@class, '_acap') and contains(@class, '_acas') and contains(@class, '_aj1-') and contains(., 'Seguir')]")
+                print("len(boton_siguiendo)",len(boton_siguiendo) )
+                
+                # Si el botón "Seguir" está presente, haga clic en él y espere 20 segundos
+                if len(boton_siguiendo) > 0 and not usuario_ya_guardado(usuario):
+                    
+                    boton_siguiendo[0].click()
+                    
+                    time.sleep(random.randint(60, 70))
+                    contador_total +=1
+                    guardar_usuario_csv(usuario)
+
+            except NoSuchElementException:
+                print("no se encontro el boton")
+                # Si el botón "Seguir" no está presente, significa que ya estás siguiendo al usuario
+                pass
+                        
+            time.sleep(random.randint(1, 2))
+
 
     scrolls += 1 
-    driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight;', followers_list)
+    driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight;', following_list)
     time.sleep(random.randint(5, 8))
-    new_scroll_height = driver.execute_script('return arguments[0].scrollHeight;', followers_list)
+    new_scroll_height = driver.execute_script('return arguments[0].scrollHeight;', following_list)
     if new_scroll_height == scroll_height:
         break
     scroll_height = new_scroll_height
@@ -155,44 +199,15 @@ while True:
 
 
 
-casillas_grandes = driver.find_elements(By.XPATH,"//div[contains(@class, 'x9f619') and contains(@class, 'xjbqb8w') and contains(@class, 'x78zum5') and contains(@class, 'x168nmei') and contains(@class, 'x13lgxp2') and contains(@class, 'x5pf9jr') and contains(@class, 'xo71vjh') and contains(@class, 'x1pi30zi') and contains(@class, 'x1swvt13') and contains(@class, 'xwib8y2') and contains(@class, 'x1y1aw1k') and contains(@class, 'x1uhb9sk') and contains(@class, 'x1plvlek') and contains(@class, 'xryxfnj') and contains(@class, 'x1c4vz4f') and contains(@class, 'x2lah0s') and contains(@class, 'xdt5ytf') and contains(@class, 'xqjyukv') and contains(@class, 'x1qjc9v5') and contains(@class, 'x1oa3qoh') and contains(@class, 'x1nhvcw1')]")
-
-for casilla in casillas_grandes:
-    if contador_total > 50:
-        break
-
-    usuario = casilla.text.split('\n')[0]
-    nombre_usuario = casilla.text.split('\n')[1]
-    print(nombre_usuario)
-
-    if predict_gender(nombre_usuario) == "mujer":
-        print(nombre_usuario, "es mujer")
-        try:
-            
-            #boton_siguiendo = casilla.find_element(By.XPATH, ".//div[contains(@class, 'x9f619') and contains(@class, 'x1q0g3np') and contains (@class, 'xqjyukv') and contains (@class, 'x1nhvcw1') and contains (@class, 'x1oa3qoh')]//button//div[contains(text(), 'Seguir')]")
-            boton_siguiendo = casilla.find_elements(By.XPATH, ".//button[contains(@class, '_acan') and contains(@class, '_acap') and contains(@class, '_acas') and contains(@class, '_aj1-') and contains(., 'Seguir')]")
-            print("len(boton_siguiendo)",len(boton_siguiendo) )
-            
-            # Si el botón "Seguir" está presente, haga clic en él y espere 20 segundos
-            if len(boton_siguiendo) > 0 and not usuario_ya_guardado(usuario):
-                
-                boton_siguiendo[0].click()
-                
-                time.sleep(random.randint(60, 70))
-                contador_total +=1
-                guardar_usuario_csv(usuario)
-
-        except NoSuchElementException:
-            print("no se encontro el boton")
-            # Si el botón "Seguir" no está presente, significa que ya estás siguiendo al usuario
-            pass
-                      
-        time.sleep(random.randint(1, 2))
 
 
     
 
 driver.close()
+
+
+            
+
 
 
             
